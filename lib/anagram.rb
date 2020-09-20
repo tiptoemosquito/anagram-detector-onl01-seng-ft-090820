2595d15dbb6bc1706f1e21b710fc92f5af765b00
Christina Howard <clhoward33@icloud.com>
2020-09-20 19:59:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagram
+
+  @@all = []
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anagram)
+  anagram.select {|a| a.split("").sort == @anagram.split("").sort}
+  end
+
+
+end
